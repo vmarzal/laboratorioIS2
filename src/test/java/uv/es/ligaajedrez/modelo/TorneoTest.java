14e71592359f9b959885514b3440c8f79400fde7
En10 <En10@147.156.104.45>
2020-12-01 17:41:58 +0100

    tests de todas las clases del paquete modelo, y todos los metodos tienen las lineas  'fail("el test falla");'

---
+++ src/test/java/uv/es/ligaajedrez/modelo/TorneoTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uv.es.ligaajedrez.modelo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author En10
+ */
+public class TorneoTest {
+    
+    public TorneoTest() {
+    }
+
+    @Test
+    public void testAddPartidaDelTorneo() {
+         fail("el test falla");
+    }
+
+    @Test
+    public void testAddJugador() {
+         fail("el test falla");
+    }
+
+    @Test
+    public void testRemoveJugador() {
+         fail("el test falla");
+    }
+
+    @Test
+    public void testToString() {
+         fail("el test falla");
+    }
+    
+}
